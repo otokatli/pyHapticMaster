5b788855347fd0d59892d3d4cc02cc7ec7e6cb8e
Ozan Tokatli <ozan.tokatli@gmail.com>
2023-02-21 09:27:11 +0000

    reimplemented with dataclass

--- haptic_master/shaker.py
+++ haptic_master/shaker.py
@@ -1,15 +1,14 @@
+from dataclasses import dataclass
+
+@dataclass(repr=True)
 class Shaker:
-    def __init__(self, frequency1: float, frequency2: float,
-                 direction: list, posmax: float, velmax: float,
-                 accmax: float, stiffness: float, dampfactor: float,
-                 deadband: float, maxforce: float) -> None:
-        self._frequency1 = frequency1
-        self._frequency2 = frequency2
-        self._direction = direction
-        self._posmax = posmax
-        self._velmax = velmax
-        self._accmax = accmax
-        self._stiffness = stiffness
-        self._dampfactor = dampfactor
-        self._deadband = deadband
-        self._maxforce = maxforce
+    frequency1: float
+    frequency2: float
+    direction: list
+    posmax: float
+    velmax: float
+    accmax: float
+    stiffness: float
+    dampfactor: float
+    deadband: float
+    maxforce: float
