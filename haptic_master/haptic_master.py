4de99abe32b41a4d4e70886ba94f47c6702af286
Ozan Tokatli <ozan.tokatli@gmail.com>
2023-02-22 11:43:10 +0000

    communication merged into haptic master

--- haptic_master/haptic_master.py
+++ haptic_master/haptic_master.py
@@ -1,7 +1,9 @@
 import socket
 from typing import Tuple
-from .communication import send_message
+import re
 import logging
+from string import printable
+
 
 
 class HapticMaster:
@@ -15,6 +17,10 @@ class HapticMaster:
     def address(self) -> Tuple[str, int]:
         return (self._ip, self._port)
 
+    @property
+    def sock(self):
+        return self._socket
+    
     # @property
     # def inertia(self) -> float:
     #     return self._inertia
@@ -36,7 +42,7 @@ class HapticMaster:
     def disconnect(self):
         # Clear all haptic effects
         msg = 'remove all'
-        logging.info(send_message(self._socket, msg))
+        logging.info(self.send_message(msg))
 
         # Close connection
         self._socket.close()
@@ -44,12 +50,12 @@ class HapticMaster:
     def get_inertia(self) -> float:
         msg = 'get inertia'
 
-        return float(send_message(self._socket, msg))
+        return float(self.send_message(msg))
     
     def set_inertia(self, value: float) -> bool:
         msg = 'set inertia ' + str(value)
         
-        response = send_message(self._socket, msg)
+        response = self.send_message(msg)
 
         logging.info(response)
 
@@ -58,6 +64,32 @@ class HapticMaster:
     def set_state(self, device_state):
         if device_state in ['init', 'off', 'force', 'position', 'home']:
             msg = 'set state ' + device_state
-            return send_message(self._socket, msg)
+            return self.send_message(msg)
         else:
             raise ValueError('Wrong state name is given')
+        
+    def _haptic_master_message(self, msg):
+        hm_msg = [0, 0, 0, 0, 0, 0, 0, 0]
+        
+        # Convert msg to ascii dec
+        decimal_msg = [ord(c) for c in msg]
+        
+        hm_msg[3] = len(decimal_msg)
+        
+        return hm_msg + decimal_msg
+    
+    def _haptic_master_response(self, msg):
+        msg_str = ''.join(c for c in msg.decode('ascii') if c in printable)
+        
+        if re.search('[a-zA-Z]', msg_str):
+            return msg_str.replace('"', '')
+        else:
+            return msg_str
+
+    def send_message(self, msg: str) -> str:
+        self._socket.sendall(bytearray(self._haptic_master_message(msg)))
+
+        try:
+            return self._haptic_master_response(self._socket.recv(1024))
+        except Exception as e:
+            raise e
