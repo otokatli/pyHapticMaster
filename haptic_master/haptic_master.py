01bc02e22d93c2341a1e2fad8a305e36eb902294
Ozan Tokatli <ozan.tokatli@gmail.com>
2023-02-23 11:41:20 +0000

    implemented return bool

--- haptic_master/haptic_master.py
+++ haptic_master/haptic_master.py
@@ -61,10 +61,7 @@ class HapticMaster:
     def set_coulombfriction(self, value: float) -> bool:
         msg = 'set coulombfriction ' + str(value)
 
-        if 'Coulomb friction set' in self.send_message(msg):
-            return True
-        else:
-            return False
+        return 'Coulomb friction set' in self.send_message(msg
 
     def get_measpos(self) -> list:
         msg = 'get measpos'
