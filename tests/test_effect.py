9ba9b0923ace7f38146a867d8d476d7444ec8ca6
Ozan Tokatli <ozan.tokatli@gmail.com>
2023-02-23 11:58:20 +0000

    pythonic variable names

--- tests/test_effect.py
+++ tests/test_effect.py
@@ -29,57 +29,57 @@ class TestHapticMaster(unittest.TestCase):
         dampglobal = False
 
         # Create a spring instance
-        mySpring = Spring(name='mySpring', robot=robot)
+        my_spring = Spring(name='mySpring', robot=robot)
 
         # Create the spring on the HapticMaster controller
-        self.assertTrue(mySpring.create())
+        self.assertTrue(my_spring.create())
 
         # Set position of the spring and read it from the robot
-        self.assertTrue(mySpring.set_pos(position))
-        self.assertEqual(mySpring.get_pos(), position)
+        self.assertTrue(my_spring.set_pos(position))
+        self.assertEqual(my_spring.get_pos(), position)
 
         # Set velocity of the spring and read it from the robot
-        self.assertTrue(mySpring.set_vel(velocity))
-        self.assertEqual(mySpring.get_vel(), velocity)
+        self.assertTrue(my_spring.set_vel(velocity))
+        self.assertEqual(my_spring.get_vel(), velocity)
 
         # Set attitude of the spring and read it from the robot
-        self.assertTrue(mySpring.set_att(attitude))
-        # self.assertAlmostEqual(mySpring.get_att(), attitude, places=5)
+        self.assertTrue(my_spring.set_att(attitude))
+        # self.assertAlmostEqual(my_spring.get_att(), attitude, places=5)
 
         # Enable the spring
-        self.assertTrue(mySpring.set_enable())
-        self.assertEqual(mySpring.get_enabled(), True)
+        self.assertTrue(my_spring.set_enable())
+        self.assertEqual(my_spring.get_enabled(), True)
 
         # Disable the spring
-        self.assertTrue(mySpring.set_disable())
-        self.assertEqual(mySpring.get_enabled(), False)
+        self.assertTrue(my_spring.set_disable())
+        self.assertEqual(my_spring.get_enabled(), False)
 
         # Set stiffness and read it from the robot
-        self.assertTrue(mySpring.set_stiffness(stiffness))
-        self.assertEqual(mySpring.get_stiffness(), stiffness)
+        self.assertTrue(my_spring.set_stiffness(stiffness))
+        self.assertEqual(my_spring.get_stiffness(), stiffness)
 
         # # Set damping factor and read it from the robot
-        self.assertTrue(mySpring.set_dampfactor(dampfactor))
-        self.assertEqual(mySpring.get_dampfactor(), dampfactor)
+        self.assertTrue(my_spring.set_dampfactor(dampfactor))
+        self.assertEqual(my_spring.get_dampfactor(), dampfactor)
 
         # # Set deadband length and read it from the robot
-        self.assertTrue(mySpring.set_deadband(deadband))
-        self.assertEqual(mySpring.get_deadband(), deadband)
+        self.assertTrue(my_spring.set_deadband(deadband))
+        self.assertEqual(my_spring.get_deadband(), deadband)
 
         # # Set spring direction and read it from the robot
-        self.assertTrue(mySpring.set_direction(direction))
-        self.assertEqual(mySpring.get_direction(), direction)
+        self.assertTrue(my_spring.set_direction(direction))
+        self.assertEqual(my_spring.get_direction(), direction)
 
         # # Set spring's maximum force and read it from the robot
-        self.assertTrue(mySpring.set_maxforce(deadband))
-        self.assertEqual(mySpring.get_maxforce(), deadband)
+        self.assertTrue(my_spring.set_maxforce(deadband))
+        self.assertEqual(my_spring.get_maxforce(), deadband)
 
         # Set spring's global damping and read it from the robot
-        self.assertTrue(mySpring.set_dampglobal(True))
-        self.assertTrue(mySpring.get_dampglobal())
+        self.assertTrue(my_spring.set_dampglobal(True))
+        self.assertTrue(my_spring.get_dampglobal())
 
         # Remove effect
-        self.assertTrue(mySpring.remove())
+        self.assertTrue(my_spring.remove())
 
         robot.disconnect()
 
@@ -93,16 +93,16 @@ class TestHapticMaster(unittest.TestCase):
         robot.connect()
 
         # Create a damper instance
-        myDamper = Damper(name='myDamper', robot=robot)
+        my_damper = Damper(name='myDamper', robot=robot)
 
         # Create the damper on the HapticMaster controller
-        self.assertTrue(myDamper.create())
+        self.assertTrue(my_damper.create())
 
         dampcoef = [0.2, 0.43, 0.59]
 
         # Set the damper's damping coefficient and read it from the robot
-        self.assertTrue(myDamper.set_dampcoef(dampcoef))
-        self.assertEqual(myDamper.get_dampcoef(), dampcoef)
+        self.assertTrue(my_damper.set_dampcoef(dampcoef))
+        self.assertEqual(my_damper.get_dampcoef(), dampcoef)
 
         robot.disconnect()
 
@@ -116,16 +116,16 @@ class TestHapticMaster(unittest.TestCase):
         robot.connect()
 
         # Create a damper instance
-        myBiasForce = BiasForce(name='myBiasForce', robot=robot)
+        my_bias_force = BiasForce(name='myBiasForce', robot=robot)
 
         # Create the damper on the HapticMaster controller
-        self.assertTrue(myBiasForce.create())
+        self.assertTrue(my_bias_force.create())
 
         force = [0.192, 0.233, 0.995]
 
         # Set the bias force's value and read it from the robot
-        self.assertTrue(myBiasForce.set_force(force))
-        self.assertEqual(myBiasForce.get_force(), force)
+        self.assertTrue(my_bias_force.set_force(force))
+        self.assertEqual(my_bias_force.get_force(), force)
 
         robot.disconnect()
 
